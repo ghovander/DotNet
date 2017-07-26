9ffbd6985796fd6eaf3ee24b55024f2bca80c8be
ghovander <ghovander@gmail.com>
2017-07-25 23:07:26 -0700

    Added DbContext for EF Core w/o Connection String

---
+++ Vega/Persistence/VegaDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+        }
+    }
+}
